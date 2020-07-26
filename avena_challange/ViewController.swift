2c84dfb53e80361b5563800634b4d1c6ea70e28c
bkomerkaya <omer.kaya@bktomorrow.com>
2020-07-26 12:02:01 +0300

    First commit with empty project

---
+++ avena_challange/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  avena_challange
+//
+//  Created by Omer Kaya on 26.07.2020.
+//  Copyright © 2020 Omer Kaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
